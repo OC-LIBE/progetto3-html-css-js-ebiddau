03da0854e17b70a2199c150566ccc96ef08bbbbf
ebiddau <emabiddau@gmail.com>
2025-06-04 09:42:05 +0200

    finite le schede: bottone adotta interattivo, visualizza età e specie. Iniziato (e commentato perché non funziona) il meccanismo di selezione: rimuovere se specie non coincide. Specie si sceglie da i bottoni, a cui è stato aggiunto un event listener

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ async function getPetsData() {
     return petsData;
 }
 
+let speciesSelection = "cat";
 
 async function displayPets() {
     const pets = await getPetsData();
@@ -15,32 +16,85 @@ async function displayPets() {
 
     console.log(template);
 
+    /*for (let index = 0; index < pets.length; index++) {
+        const pet = pets[index];
+
+        if (speciesSelection != pet.species) {
+            pets.delete(pet);
+        }
+        
+    }*/
+
 
     pets.forEach(pet => {
-            const clone = template.content.cloneNode(true);
-            console.log(pet)
+        
+        const clone = template.content.cloneNode(true);
+        console.log(pet);
 
-            // qui modifichiamo il template
+        // qui modifichiamo il template
 
-            // aggiungiamo l'articolo alla pagina
+        // aggiungiamo l'articolo alla pagina
 
-            const image = clone.querySelector('.animal-card-photo img');
-            image.src=pet.photo;
+        const image = clone.querySelector('.animal-card-photo img');
+        image.src=pet.photo;
 
-            const name = clone.querySelector('.animal-card-text h1');
-            name.textContent = pet.name;
-
-            const p = clone.querySelector('.animal-card-text p');
-            p.textContent = pet.description;
-
-            const a = clone.querySelector('.animal-card-text .adopt-button');
-            a.href = website.concat("pets/").concat(pet.id).concat("/");
+        const name = clone.querySelector('.animal-card-text h1');
+        name.textContent = pet.name;
 
 
-            wrapper.appendChild(clone);
+        const small = clone.querySelectorAll('.animal-card-text small span');
+        petAge = getPetAge(pet.birthYear);
+
+        if (petAge == 0) {
+            small[0].textContent = "less than a year old";
+        } else if (petAge == 1) {
+            small[0].textContent = petAge + " year old";
+        } else {
+            small[0].textContent = petAge + " years old";
         }
-    );
+
+        small[1].textContent = capitalize(pet.species);
+
+
+        const p = clone.querySelector('.animal-card-text p');
+        p.textContent = pet.description;
+
+        const a = clone.querySelector('.animal-card-text .adopt-button');
+        a.href = website.concat("pets/").concat(pet.id).concat("/");
+        a.textContent = "Adotta ".concat(pet.name);
+
+
+        wrapper.appendChild(clone);
+    });
 
 }
 
 displayPets()
+
+
+function getPetAge(birthYear) {
+    const d = new Date();
+    let currentYear = d.getFullYear();
+
+    return currentYear - birthYear;
+}
+
+
+function capitalize(species) {
+    return species[0].toUpperCase() + species.slice(1);
+}
+
+
+function navigate() {
+    selection = this.id;
+    console.log(selection);
+}
+
+
+navButtons = document.getElementsByClassName("navBt");
+for (let index = 0; index < navButtons.length; index++) {
+    const navButton = navButtons[index];
+
+    navButton.addEventListener("click", navigate);
+    
+}
